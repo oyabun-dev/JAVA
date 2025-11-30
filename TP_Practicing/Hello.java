39a3f44abfa0579ca231857083919f696a31c976
oyabun-dev <khalifa@orvynlab.com>
2025-11-30 01:57:06 +0000

    Do Exo 3

--- TP_Practicing/Hello.java
+++ TP_Practicing/Hello.java
@@ -1,4 +1,25 @@
 public class Hello {
+    // exo3 2
+    public static int somme (int[] tab) {
+        int r = 0;
+        for (byte i = 0; i < tab.length; i++) {
+            r += tab[i];
+        }
+        return r;
+    }
+    public static int produit (int[] tab) {
+        int r = 1;
+        for (byte i = 0; i < tab.length; i++) {
+            r *= tab[i];
+        }
+        return r;
+    }
+    
+    public static int moyenne (int[] tab) {
+        int r = somme(tab) / tab.length;
+        return r;
+    }
+
     public static void main(String[] args) {
         // TD 1 Familiariation avec Java
         // Exo 1:
@@ -23,13 +44,29 @@ public class Hello {
             //     System.out.println("Hello " + name + " !");
             // }
         // 3. Modifiez maintenant le programme pour que pour chaque nom donné sur la ligne de commande, il affiche une ligne de salutation.
-            if (args.length == 0) {
-                System.out.println("Hello World !");
-            } else {
-                for (byte i = 0; i < args.length; i++) {
-                    String name = args[i];
-                    System.out.println("Hello " + name + " !");
-                }
-            }
+            // if (args.length == 0) {
+            //     System.out.println("Hello World !");
+            // } else {
+            //     for (byte i = 0; i < args.length; i++) {
+            //         String name = args[i];
+            //         System.out.println("Hello " + name + " !");
+            //     }
+            // }
+
+            // Exo 3:
+            // 1. Écrivez un programme Somme effectuant la somme du tableau : {1, 23, 12, -4, 6, 89, 2} et affichant le résultat.
+                // int[] tab = {1, 23, 12, -4, 6, 89, 2};
+                // int sum = 0;
+                // for (byte i = 0; i < tab.length; i++) {
+                //     sum += tab[i];
+                // }
+                // System.out.println(sum);
+            // 2. Définissez une méthode static, qui prendra en argument le tableau, qui effectuera le calcul et qui retournera la valeur obtenue. main ne sera responsable que de l'appel de la fonction et de l'affichage. Le prototype de la méthode est : public static int somme(int[] tab)
+                int[] tab = {1, 23, 12, -4, 6, 89, 2};
+                System.out.println(somme(tab));
+                System.out.println(produit(tab));
+                System.out.println(moyenne(tab));
+
+            // 3. Ajoutez la méthode produit et la méthode moyenne au programme.
     }
 }
